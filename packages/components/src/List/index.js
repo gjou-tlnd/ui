9f226af8790dc7ac7aa5c7f624b231ac086c6b29
Fabien Rassinier <frassinier@talend.com>
2020-05-27 09:52:10 +0200

    List exports

--- packages/components/src/List/index.js
+++ packages/components/src/List/index.js
@@ -1,3 +1,11 @@
 import List from './List.component';
+import ListComposition, { hooks } from './ListComposition'
+import Toolbar from './Toolbar';
+
+ListComposition.hooks = hooks;
+
+List.ListComposition = ListComposition;
+
+List.Toolbar = Toolbar;
 
 export default List;
